9a5f02511cfd483355c54fa560c69dad8b8fb91b
Victor Gonzalez <gonzalez.victor.javier@outlook.com>
2022-01-14 15:06:12 -0300

    I implemented a DatePickerDialog at the button with a toat message.

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.ageinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.selectDateButton) as Button
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
         button.setOnClickListener{
-            Toast.makeText(this, "Button works", Toast.LENGTH_SHORT).show()
+            DatePickerDialog(this,DatePickerDialog.OnDateSetListener{
+                    view, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this, "Date selected", Toast.LENGTH_SHORT).show()
+            }
+                ,year
+                ,month
+                ,day).show()
         }
+
     }
-
-
 }
